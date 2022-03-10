3d692ef64ea83307a238617e9af1e627845e7e87
PedroMiguelVilar <39820@ufp.edu.pt>
2022-03-10 18:42:10 +0000

    Changes

---
+++ src/edu/ufp/inf/lp2/_03PL/Person.java
@@ -0,0 +1,35 @@
+package  edu.ufp.inf.lp2._03PL;
+
+
+
+public class Person {
+
+  private String idNumebr;
+
+  private String name;
+
+  private String adress;
+
+  private DateLP birth;
+
+  public int age() {
+  return 0;
+  }
+
+  public boolean olderThan(Person p) {
+  return false;
+  }
+
+  public String toString() {
+  return null;
+  }
+
+  public boolean equals(Object o) {
+  return false;
+  }
+
+  public int hashCode() {
+  return 0;
+  }
+
+}
